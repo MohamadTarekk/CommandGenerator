6503b5ee4e685e319c8cdf68aef131c20d5614b3
mohamadtarek_ <mohamadtarekelsherbeny@gmail.com>
2018-08-31 14:56:09 +0200

    Global Exception Hanlding

--- CommandGenerator/Program.cs
+++ CommandGenerator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using log4net;
+using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CommandGenerator
@@ -13,7 +15,40 @@ namespace CommandGenerator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            AppDomain currentDomain = default(AppDomain);
+            currentDomain = AppDomain.CurrentDomain;
+            // Handler for unhandled exceptions.
+            currentDomain.UnhandledException += GlobalUnhandledExceptionHandler;
+            // Handler for exceptions in threads behind forms.
+            Application.ThreadException += GlobalThreadExceptionHandler;
+ 
+            //#comment handle any exception from application
+            Application.ThreadException += new ThreadExceptionEventHandler(MyCommonExceptionHandlingMethod);
+
             Application.Run(new CommandGenerator());
+        }
+
+        private static void MyCommonExceptionHandlingMethod(object sender, ThreadExceptionEventArgs e)
+        {
+            //#comment use your logger
+            FileParser.LogException(e.Exception.StackTrace);
+            throw new NotImplementedException();
+        }
+        private static void GlobalUnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = (Exception)e.ExceptionObject;
+            ILog log = LogManager.GetLogger(typeof(Program)); //Log4NET nuget
+            log.Error(ex.Message + "\n" + ex.StackTrace);
+        }
+
+        private static void GlobalThreadExceptionHandler(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            Exception ex = default(Exception);
+            ex = e.Exception;
+            ILog log = LogManager.GetLogger(typeof(Program));
+            log.Error(ex.Message + "\n" + ex.StackTrace);
         }
     }
 }
